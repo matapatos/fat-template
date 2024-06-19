99fa20c8bc0ed00b0e2dce1619c06d129a1a18d6
matapatos <andre_gil22@hotmail.com>
2024-06-19 17:32:45 +0100

    fix: Missing css error

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import os
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 from routers.api import router as api_router
 from routers.pages import router as pages_router
 
@@ -20,7 +21,7 @@ def mount_dirs(app: FastAPI) -> None:
     Mounts all file directories to be served
     """
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    app.mount("/css", StaticFiles(directory=f"{current_dir}/dist/css"), name="styles")
+    app.mount("/css", StaticFiles(directory=f"{current_dir}/dist/css", check_dir=False), name="styles")
     app.mount("/js", StaticFiles(directory=f"{current_dir}/dist/js"), name="scripts")
     app.mount(
         "/static",
--- app/routers/pages.py
+++ app/routers/pages.py
@@ -5,10 +5,11 @@ Holds endpoints for HTML pages
 import json
 import os
 
-from config import configs
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from config import configs
 
 router = APIRouter()
 templates = Jinja2Templates(directory=configs.views_dir)
@@ -16,12 +17,18 @@ templates = Jinja2Templates(directory=configs.views_dir)
 
 def bundle_urls_for(name: str) -> list[str]:
     bundles = []
+    if "." not in name:
+        raise Exception(
+            f"Missing dot in bundle name. Expected something like '{name}.js' or '{name}.css' but got '{name}'"
+        )
+
+    name, type_ = name.split(".")
     if "bundle_entrypoints" not in templates.env.globals:
         with open(configs.bundle_entrypoints_file, "rb") as file:
             templates.env.globals.update(bundle_entrypoints=json.load(file))
 
     entrypoints = templates.env.globals["bundle_entrypoints"]
-    return entrypoints.get(name, {}).get("js", [])
+    return entrypoints.get(name, {}).get(type_, [])
 
 
 templates.env.globals.update(bundle_urls_for=bundle_urls_for)
