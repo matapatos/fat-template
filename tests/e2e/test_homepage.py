53584c6349b115dcd3ef11c8779cd73dadb00bbf
matapatos <andre_gil22@hotmail.com>
2024-07-01 14:38:21 +0100

    test: Add e2e homepage tests

--- tests/e2e/test_homepage.py
+++ tests/e2e/test_homepage.py
@@ -5,7 +5,38 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_has_title(page: Page):
+def test_title(page: Page):
     """Tests homepage title"""
     page.goto("http://localhost:8000/")
-    expect(page).to_have_title(re.compile("FAT boilerplate"))
+    expect(page).to_have_title(re.compile("FAT template"))
+
+
+def test_text(page: Page):
+    """Tests homepage text"""
+    page.goto("http://localhost:8000/")
+    expect(page.get_by_role("heading", name="Let's start coding 😊?")).to_be_visible()
+    expect(
+        page.get_by_test_id("no-text"), page.get_by_test_id("yes-text")
+    ).to_be_hidden()
+
+
+def test_click_yes_button(page: Page):
+    """
+    Tests that yes-text shows
+    When clicking in 'Yes' button
+    """
+    page.goto("http://localhost:8000/")
+    page.get_by_role("button", name="Yes").click()
+    expect(page.get_by_test_id("yes-text")).to_be_visible()
+    expect(page.get_by_test_id("no-text")).to_be_hidden()
+
+
+def test_click_no_button(page: Page):
+    """
+    Tests that no-text shows
+    When clicking in 'No' button
+    """
+    page.goto("http://localhost:8000/")
+    page.get_by_role("button", name="No").click()
+    expect(page.get_by_test_id("no-text")).to_be_visible()
+    expect(page.get_by_test_id("yes-text")).to_be_hidden()
